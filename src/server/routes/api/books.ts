6182bbe4f490f7221ce757b4c70ac994445b0e3c
Zishan Ali <zishanali06@gmail.com>
2019-04-23 18:27:17 -0500

    setup first route in api endpoint, tested and working

--- src/server/routes/api/books.ts
+++ src/server/routes/api/books.ts
@@ -1,9 +1,16 @@
 import * as express from 'express';
+import knex from '../../db';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-
+router.get('/', async (req, res, next) => {
+    try {
+        let data = await knex('books').select('title');
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 });
 
 export default router;
